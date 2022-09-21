eb672f895ace769ff74a2577ea3643157b18ddfd
unknown <Dmitiy_yar@mail.ru>
2022-09-21 13:08:38 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 50.
+//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+int[,] GetMatrix(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+
+        {
+            matrix[i, j] = new Random().Next(99);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < inputMatrix.GetLength(1); m++)
+        {
+            Console.Write(inputMatrix[i, m] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrix = GetMatrix(10, 10);
+PrintMatrix(matrix);
+
+Console.WriteLine("Введите номер строки");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+if (rows < matrix.GetLength(0) && columns < matrix.GetLength(1)) Console.WriteLine(matrix[rows, columns]);
+else Console.WriteLine("Нет такого элемента");
