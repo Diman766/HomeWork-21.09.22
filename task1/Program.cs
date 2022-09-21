ccf2b20af0aabf89b9c9928d6d920b86cd8287aa
unknown <Dmitiy_yar@mail.ru>
2022-09-21 12:28:49 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 47. 
+// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.WriteLine("Введите количество строк");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] GetMatrix(int m, int n)
+{
+    double[,] matrix = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble() * 10 * Remove0(), 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(double[,] inputMatrix)
+{
+    for (int i = 0; i < inputMatrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < inputMatrix.GetLength(1); m++)
+        {
+            Console.Write(inputMatrix[i, m] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintMatrix(GetMatrix(rows, columns));
+
+int Remove0()
+{
+    int a = new Random().Next(-1,2);
+    while (a == 0) a = new Random().Next(-1, 2);
+    return a;
+}
+
